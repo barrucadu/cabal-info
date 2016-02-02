80942181bb44fe0ce565bee6664a6892bb892127
Michael Walker <mike@barrucadu.co.uk>
2016-02-02 18:55:40 +0000

    Better pretty-printing of descriptions

--- Describe.hs
+++ Describe.hs
@@ -1,8 +1,8 @@
 -- | Pretty-printing packages.
 module Describe where
 
-import Data.List (delete)
-import Data.Maybe (maybeToList)
+import Data.List (elemIndex, delete, intercalate, isInfixOf, isPrefixOf)
+import Data.Maybe (fromJust, maybeToList)
 import Distribution.PackageDescription (PackageDescription(..), Library, Executable, TestSuite, Benchmark)
 
 import Fields
@@ -21,7 +21,47 @@ data Description =
 
 -- | Pretty-print a package description.
 describe :: PackageDescription -> String
-describe = show . describePackage
+describe = prettyPrint . describePackage where
+  prettyPrint (Package fields rest) = intercalate "\n" $ prettyPrintFields fields : map prettyPrint rest
+  prettyPrint (Library fields) = "library\n" ++ indent 2 (prettyPrintFields fields)
+  prettyPrint (Executable name fields) = "executable " ++ name ++ "\n" ++ indent 2 (prettyPrintFields fields)
+  prettyPrint (TestSuite  name fields) = "test-suite " ++ name ++ "\n" ++ indent 2 (prettyPrintFields fields)
+  prettyPrint (Benchmark  name fields) = "benchmark "  ++ name ++ "\n" ++ indent 2 (prettyPrintFields fields)
+
+  prettyPrintFields fs = foldl pp "" $ map (\(f,v) -> (align (maxlen + 2) (f ++ ":"), v)) fs where
+    maxlen = maximum $ map (length . fst) fs
+
+    -- Pretty-print a field and value.
+    pp sofar (f, v) = sofar ++ f ++ ppv f v ++ "\n"
+
+    -- Pretty-print a value. Has some special cases.
+    ppv f v
+      -- Special case for description: just want to indent further
+      -- lines by 2 spaces, and replace blank lines with an indented
+      -- '.'.
+      | "description" `isPrefixOf` f = indentS 2 . unlines' . map (\l -> if null l then "." else l) . lines $ v
+
+      -- Special case for non-description multi-line fields: want to
+      -- indent non-first-lines to the ":" in the field name, and add
+      -- commas.
+      | "\n" `isInfixOf` v = indentS (length f - 2) $ mapLines (", " ++) v
+
+      -- Normal case is just the value.
+      | otherwise = v
+
+    -- Apply padding at the end of the line to the desired length.
+    align n s = s ++ replicate (n - length s) ' '
+
+  -- Indent every line.
+  indent n = unlines . map (replicate n ' '++) . lines
+
+  -- Indent every line after the first.
+  indentS n = mapLines (replicate n ' '++)
+
+  -- Apply a function to all but the first line.
+  mapLines f str = case lines str of
+    (x:xs) -> unlines' $ x : map f xs
+    [] -> ""
 
 -- | Produce all defined fields in a package.
 describePackage :: PackageDescription -> Description
@@ -35,25 +75,25 @@ describePackage pkg = Package fields sections where
 
 -- | Produce all defined fields in a library.
 describeLibrary :: Library -> Description
-describeLibrary = Library . getFields libraryFields getLibraryField
+describeLibrary = Library . getFields (libraryFields ++ buildInfoFields) getLibraryField
 
 -- | Produce all defined fields in an executable.
 describeExecutable :: Executable -> Description
 describeExecutable exe = Executable name fields where
   name   = getExecutableField "name" exe
-  fields = getFields (delete "name" executableFields) getExecutableField exe
+  fields = getFields (delete "name" (executableFields ++ buildInfoFields)) getExecutableField exe
 
 -- | Produce all defined fields in a test suite.
 describeTestSuite :: TestSuite -> Description
 describeTestSuite test = TestSuite name fields where
   name   = getTestSuiteField "name" test
-  fields = getFields (delete "name" testSuiteFields) getTestSuiteField test
+  fields = getFields (delete "name" (testSuiteFields ++ buildInfoFields)) getTestSuiteField test
 
 -- | Produce all defined fields in a benchmark.
 describeBenchmark :: Benchmark -> Description
 describeBenchmark bench = Benchmark name fields where
   name   = getBenchmarkField "name" bench
-  fields = getFields (delete "name" benchmarkFields) getBenchmarkField bench
+  fields = getFields (delete "name" (benchmarkFields ++ buildInfoFields)) getBenchmarkField bench
 
 -- | Get all non-empty fields.
 getFields :: [String] -> (String -> a -> String) -> a -> [(String, String)]
